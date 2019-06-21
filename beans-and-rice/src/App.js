f5ff1f252bf26afe87961e7e7d072e63038e7c25
Mark Reed <markdreed512@gmail.com>
2019-06-21 16:51:47 -0500

    header component

--- beans-and-rice/src/App.js
+++ beans-and-rice/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import ChooseMealPage from './pages/ChooseMealPage'
+import ChooseMealPage from './pages/ChooseMealPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="container">
+    <div>
+      <Header />
       <ChooseMealPage />
     </div>
   );
